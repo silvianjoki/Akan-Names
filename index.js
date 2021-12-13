9f3b186d86bd96661a0063b0940e7058ddaf29b6
Silvia Njoki <10silvianjoki@gmail.com>
2021-12-14 00:11:19 +0300

    change JS

--- index.js
+++ index.js
@@ -1,54 +1,12 @@
-function submitForm(event) {
-    event.preventDefault();
-    var gender = document.forms["akanForm"] ["gender"].value;
-    var day = document.forms["akanForm"] ["day"]. value;
-    var birthYear = document.forms["akanForm"] ['birthYear'].value;
-    var birthMonth = document.forms ["akanForm"] ["birthMonth"].value
-    var akanName = "";
+var CC, YY, MM, DD, d, dayValue;
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
-    var days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-    ];
-
-    var femaleNames = [
-        "Akosua",
-        "Adwoa",
-        "Abenaa",
-        "Akua",
-        "Yaa",
-        "Afua",
-        "Ama",
-    ];
-
-    var maleNames = [
-        "Kwasi",
-        "Kwadwo",
-        "Kwabena",
-        "Kwaku",
-        "Yaw",
-        "Kofi",
-        "Kwame",
-    ];
-
-    var dateObject = new Date(datestring);
-    var day = dateObject.getDay(0);
-
-    if (day != null && gender != null ) {
-        if (gender === "female") {
-            akanName = femaleNames [day];
-        }
-        else {
-            akanName = maleNames [day];
-        }
-
-
-    }
-    document.getElementById("akanName").innerHTML = akanName;
-
+function calculateDayValue(){
+    year = document.getElementById("year").value;
+    YY= parseInt (year.substring (2,4));
+    MM = parseInt (document.getElementById ("birth-month").value);
+    DD = parseInt (document.getElementById ("birth-day"). value); 
+    d = ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7)
 }
