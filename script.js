ae8c709069d14bfcf177bba5639ad8bf57be18b5
Silvia Njoki <10silvianjoki@gmail.com>
2021-12-15 17:42:38 +0300

    add js changes

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+// defining the variables
 var dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 var femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 
-
+// calculating the day
 function calculateDayValue(){
     year = document.getElementById("year").value;
     CC = parseInt (year.substring (0,2));
     YY = parseInt (year.substring (2,4));
     MM = parseInt (document.getElementById ("month").value);
-    DD = parseInt (document.getElementById ("day"). value); 
-    d = ( (CC/4) -2*CC+1) + ( (5*YY/4)) + ((26*(MM+1)/10) + DD)%7;
+    DD = parseInt (document.getElementById ("date"). value); 
+    d = (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
     console.log(d)
     return (Math.floor(d));
 }
