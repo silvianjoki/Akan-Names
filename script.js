a56d38429932f8db552ce778ce41727e5c71dd1b
Silvia Njoki <10silvianjoki@gmail.com>
2021-12-15 17:35:05 +0300

    changes in js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-// var button = document.getElementById ("button")
-// on click = "function to execute()"
-//     console.log(birthday);
-
-object.onclick = function() {myScirpt}
-
-
-    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    var femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
-    var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+var dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 
 
 function calculateDayValue(){
@@ -22,73 +15,39 @@ function calculateDayValue(){
 }
 
 // defining the gender
-function getGender (){
+function getGender (dayValue){
+    var gender;
     var genders = document.getElementsByName ("gender")
     if (genders[0].checked == true){
-        var gender = "female";
+        gender = "female";
     }
     else if (genders[1].checked == true){
-        var gender = "male";
+        gender = "male";
     }
     else{
         return false;
     }
-}
-switch(gender){
-    case "female":
-    if (dayValue === 1){
-        alert('Born on' + dayNames[0] + ' so your Akan name is ' +femaleNames[0]+"!");
+    var akanName;
+    var day = dayNames [dayValue]
+    switch(gender) {
+        case "female":
+            akanName = femaleNames[dayValue];
+            break
+        case "male":
+            akanName = maleNames [dayValue];
+            break
+        default:
+            alert("Enter correct values");
+            break;
     }
-    else if (dayValue == 2) {
-        alert('Born on' + dayNames[1] + ' so your Akan name is ' +femaleNames[1]+"!");
+    return [akanName, day];
+        }
+    // finding akanName
+    function getAkanName () {
+        var dayValue = calculateDayValue();
+        console.log(getGender(dayValue))
+        var name_day = getGender(dayValue);
+        alert("You were born on " + name_day[1] + " and Your akan name is " + name_day[0]+"!");
+        span = document.getElementById ("akanName");
+        span.texContent = name_day [0];
     }
-    else if (dayValue == 3) {
-        alert('Born on' + dayNames[2] + ' so your Akan name is ' +femaleNames[2]+"!");
-    }
-    else if (dayValue == 4) {
-        alert('Born on' + dayNames[3] + ' so your Akan name is ' +femaleNames[3]+"!");
-    }
-    else if (dayValue == 5) {
-        alert('Born on' + dayNames[4] + ' so your Akan name is ' +femaleNames[4]+"!");
-    }
-    else if (dayValue == 6) {
-        alert('Born on' + dayNames[5] + ' so your Akan name is ' +femaleNames[5]+"!");
-    }
-    else if (dayValue == 0) {
-        alert("Born on " + dayNames[6] + ' so your Akan name is ' +femaleNames[6]+"!");
-    }
-    
-    break;
-    case "male":
-    if (dayValue == 1){
-        alert(' Born on '  +dayNames[0] + ' so Your akan name is ' +maleNames[0]+"!");
-    }
-    else if (dayValue == 2){
-        alert(' Born on ' + dayNames[1] + ' so your Akan name is ' +maleNames[1]+"!");
-    }
-    else if (dayValue == 3){
-        alert(' Born on ' + dayNames[2] + ' so your Akan name is ' +maleNames[2]+"!");
-    }
-    else if (dayValue == 4){
-        alert(' Born on ' + dayNames[3] + ' so your Akan name is ' +maleNames[3]+"!");
-    }
-    else if (dayValue == 5){
-        alert(' Born on ' + dayNames[4] + ' so your Akan name is ' +maleNames[4]+"!");
-    }
-    else if (dayValue == 6){
-        alert(' Born on ' + dayNames[5] + ' so your Akan name is ' +maleNames[5]+"!");
-    }
-    else if (dayValue == 0){
-        alert(' Born on ' + dayNames[6] + ' so your Akan name is ' +maleNames[6]+"!");
-    }
-    break
-    default:
-    
-    }
-
-    // finding the akan name
-
-function nameFind () {
-    dayValue = calculateDayValue();
-    getGender();
-}
